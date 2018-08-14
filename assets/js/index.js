d11ead4e357d8317e032f5a3af582a83a7419756
jeon-sora <jsoraking@naver.com>
2018-08-14 19:17:29 +0900

    스크립트 수정 및 애니메이션적용

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,78 +1,69 @@
 $(document).ready(function () {
+  //애니메이션
+  action = new WOW(
+    {
+      boxClass: 'action'
+    }
+  )
+  action.init();
+
+
+  //풀페이지
   $('.js-fullpage-wrap').fullpage({
     navigation: true,
     lockAnchors: false,
-    //섹션을 이동할때 마다, 콜백
-    onLeave: function (index, nextIndex, direction) {
-      var headerColor = $('.js-header').hasClass('hd-white');
-      //header 색상이 흰색이 아닐때 (첫번째 섹션일 때)
-      if (headerColor == false) {
-        // 두번째 섹션으로 내릴 경우
-        if (nextIndex == 2 && direction == 'down') {
-          $('.js-header').toggleClass('hd-white');
-        }
-      }
-    }
-  });
-
-  var hdMenu = $('.hd-menu').hasClass('active');
-  var hdColor = $('.js-header').hasClass('hd-white');
-  var firstSection = $('.section.active').hasClass('first');
-  var winWidth = $(window).width();
-  //var hdMenuArea = $('.hd-menu-area').hasClass('active');
-
-  //첫번째 섹션일때만
-  if (hdColor == true && firstSection == true) {
-    $('.js-header').removeClass('hd-white');
-
-    $(window).on('load resize', function () {
-      if (winWidth < 990) {
-        $(this).addClass('hd-white');
-      }
-      else if (winWidth > 990) {
-        $(this).removeClass('hd-white');
+    scrollBar:true,
+    //브라우저 창의 크기가 바뀐 뒤에 콜백
+    afterResize: function (width, height) {
+      //일단 패쓰
+    },
+    //구역을 불러오고 나서 스크롤이 끝나면 콜백
+    afterLoad: function (origin, direction) {
+      // 첫번째 섹션 일 때,
+      if (origin == 'slide1') {
+        $('.js-header').removeClass('hd-white');
         $('.js-header').mouseenter(function () {
           $(this).addClass('hd-white');
         }).mouseleave(function () {
           $(this).removeClass('hd-white');
         });
       }
-    });
+      // 첫번째 섹션이 아닐 때,
+      else if (origin != 'slide1' || direction == 'down') {
+        $('.js-header').addClass('hd-white');
+      }
+    },
+    //사용자가 구역을 떠나고 새로운 구역으로 이동하는 와중에 콜백
+    onLeave: function (index, origin, direction) {
+      //첫번째 섹션일 때,
+      if (origin == '1') {
+        $('.js-header').removeClass('hd-white');
+      }
+      //첫번째 섹션이 아닐 때,
+      else if (origin != '1') {
+        $('.js-header').mouseleave(function () {
+          $(this).addClass('hd-white');
 
-  }
-  //첫번째 섹션 이후로는~
-  else if (firstSection == false) {
-    $('.js-header').addClass('hd-white');
-    //첫번째 섹션 아닐때는 mouse leave white add
-    $('.js-header').mouseleave(function () {
-      $(this).addClass('hd-white');
-    })
-  }
+        });
+      }
+    }
+  });
+
+  //헤더때문에
+  var hdMenu = $('.hd-menu-area').hasClass('active');
 
   //모바일메뉴 열기 버튼
-  $('.ui.js-hd-btn').click(function () {
+  $('.js-hd-menu-btn').click(function () {
     if (hdMenu == false) {
-      $('.header').addClass('active');
-      //$('.hd-menu-area').addClass('active');
+      $('.js-header, .hd-menu-area').addClass('active');
     }
   });
   //모바일 메뉴 닫기 버튼
-  $('.user-btn__item.close').click(function () {
-    if (hdMenuArea == true) {
-      $('.header').removeClass('active');
-      $('.hd-menu-area').removeClass('active');
+  $('.js-hd-close-btn').click(function () {
+    if (hdMenu == false) {
+      $('.js-header, .hd-menu-area').removeClass('active');
     }
   });
-
-  //리사이즈될때마다,
-  $(window).on('load resize', function () {
-    if (winWidth > 990) {
-      $('.hedaer').removeClass('active');
-    }
-    else if (winWidth < 990) {
-      $('.hedaer').addClass('active');
-    }
-  })
 
   //언어 선택 드롭다운
   $('.hd-lang .ui.dropdown').dropdown();
